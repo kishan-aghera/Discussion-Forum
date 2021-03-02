3291a40ba09ec2eb71997ed0cf4ecab7e3040e18
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-03-02 10:41:26 +0530

    Configured controllers

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,18 +1,21 @@
 class DiscussionsController < ApplicationController
   before_action :set_discussion, only: %i[ show edit update destroy ]
+  before_action :find_channels, only: %i[ index show new edit ]
+  before_action :authenticate_user!, except: %i[ index show ]
 
   # GET /discussions or /discussions.json
   def index
-    @discussions = Discussion.all
+    @discussions = Discussion.all.order("created_at DESC")
   end
 
   # GET /discussions/1 or /discussions/1.json
   def show
+    @discussions = Discussion.all.order("created_at DESC")
   end
 
   # GET /discussions/new
   def new
-    @discussion = Discussion.new
+    @discussion = current_user.discussions.build
   end
 
   # GET /discussions/1/edit
@@ -21,7 +24,7 @@ class DiscussionsController < ApplicationController
 
   # POST /discussions or /discussions.json
   def create
-    @discussion = Discussion.new(discussion_params)
+    @discussion = current_user.discussions.build(discussion_params)
 
     respond_to do |format|
       if @discussion.save
@@ -57,6 +60,10 @@ class DiscussionsController < ApplicationController
   end
 
   private
+
+    def find_channels
+      @channels = Channel.all.order("created_at DESC")
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_discussion
       @discussion = Discussion.find(params[:id])
